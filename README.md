98f85a8f90602d4c1089a693825d526adf152247
punytan <punytan@gmail.com>
2017-09-26 04:08:39 +0900

    impl

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # NAME
 
-Emoji::NationalFlag - Blah blah blah
+Emoji::NationalFlag - convert from country code to national flag emoji
 
 # SYNOPSIS
 
-    use Emoji::NationalFlag;
+    use Emoji::NationalFlag qw/ code2flag flag2code /;
+    use utf8;
+
+    is code2flag('jp'), "🇯🇵";
+    is code2flag('jp'), "\x{1F1EF}\x{1F1f5}";
+
+    is flag2code("🇯🇵"), 'jp';
+    is flag2code("\x{1F1EF}\x{1F1f5}"), 'jp';
 
 # DESCRIPTION
 
-Emoji::NationalFlag is
+Emoji::NationalFlag is a module to convert from country code (ISO 3166-1 alpha-2) to national flag emoji, and vice versa
+
+# METHODS
+
+## code2flag($iso\_3166\_1\_alpha\_2\_code): Optional\[NationalFlagEmoji\]
+
+This method returns national flag emoji if the supplied code (case insensitive) is valid, otherwise returns `undef`
+
+## flag2code($decoded\_national\_flag\_emoji): Optional\[lc(CountryCodeAlpha-2)\]
+
+This method returns lower case of ISO 3166-1 alpha-2 country code if the supplied emoji is valid, otherwise returns `undef`
 
 # AUTHOR
 
@@ -24,3 +41,5 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 # SEE ALSO
+
+cut
--- lib/Emoji/NationalFlag.pm
+++ lib/Emoji/NationalFlag.pm
@@ -1,8 +1,36 @@
 package Emoji::NationalFlag;
-
 use strict;
-use 5.008_005;
+use warnings;
 our $VERSION = '0.01';
+use Locale::Country 'all_country_codes';
+
+use Exporter 'import';
+our @EXPORT_OK = qw(
+    code2flag
+    flag2code
+);
+
+our %A2SYM = map { (chr 0x61 + $_) => (pack U => 0x1f1e6 + $_) } 0 .. 25;
+
+our %FLAG2CODE
+    = reverse our %CODE2FLAG
+    = map { $_ => join "", map { $A2SYM{$_} } split //, $_ } map { lc($_) } all_country_codes();
+
+sub code2flag {
+    my ($code) = @_;
+    if (defined(my $flag = $CODE2FLAG{lc($code // '')})) {
+        return $flag;
+    }
+    return;
+}
+
+sub flag2code {
+    my ($flag) = @_;
+    if (defined(my $code = $FLAG2CODE{$flag // ''})) {
+        return lc $code;
+    }
+    return;
+}
 
 1;
 __END__
@@ -11,15 +39,32 @@ __END__
 
 =head1 NAME
 
-Emoji::NationalFlag - Blah blah blah
+Emoji::NationalFlag - convert from country code to national flag emoji
 
 =head1 SYNOPSIS
 
-  use Emoji::NationalFlag;
+  use Emoji::NationalFlag qw/ code2flag flag2code /;
+  use utf8;
+
+  is code2flag('jp'), "🇯🇵";
+  is code2flag('jp'), "\x{1F1EF}\x{1F1f5}";
+
+  is flag2code("🇯🇵"), 'jp';
+  is flag2code("\x{1F1EF}\x{1F1f5}"), 'jp';
 
 =head1 DESCRIPTION
 
-Emoji::NationalFlag is
+Emoji::NationalFlag is a module to convert from country code (ISO 3166-1 alpha-2) to national flag emoji, and vice versa
+
+=head1 METHODS
+
+=head2 code2flag($iso_3166_1_alpha_2_code): Optional[NationalFlagEmoji]
+
+This method returns national flag emoji if the supplied code (case insensitive) is valid, otherwise returns C<undef>
+
+=head2 flag2code($decoded_national_flag_emoji): Optional[lc(CountryCodeAlpha-2)]
+
+This method returns lower case of ISO 3166-1 alpha-2 country code if the supplied emoji is valid, otherwise returns C<undef>
 
 =head1 AUTHOR
 
